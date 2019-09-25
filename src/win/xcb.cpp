b6c5f0b6209407437f15ea6eb98677840436168e
char <lynxy@lynxdev.xyz>
2019-09-25 19:03:48 +0000

    added more events to the mask

--- src/win/xcb.cpp
+++ src/win/xcb.cpp
@@ -45,15 +45,25 @@ xcb_window_t XCBConn::create_window(int screenNum, int x, int y, int width, int 
         return -1;
     }
 
-    // create new window on given screen, map & flush
+    xcb_screen_t screen = screens[screenNum];
 
-    uint32_t eventmask = XCB_EVENT_MASK_EXPOSURE | XCB_EVENT_MASK_FOCUS_CHANGE | XCB_EVENT_MASK_KEY_PRESS | XCB_EVENT_MASK_KEY_RELEASE;
+    // setup window values
+
+    uint32_t eventmask = XCB_EVENT_MASK_EXPOSURE 
+                        | XCB_EVENT_MASK_FOCUS_CHANGE 
+                        | XCB_EVENT_MASK_KEY_PRESS 
+                        | XCB_EVENT_MASK_KEY_RELEASE 
+                        | XCB_EVENT_MASK_BUTTON_PRESS 
+                        | XCB_EVENT_MASK_BUTTON_RELEASE 
+                        | XCB_EVENT_MASK_ENTER_WINDOW 
+                        | XCB_EVENT_MASK_LEAVE_WINDOW 
+                        | XCB_EVENT_MASK_POINTER_MOTION;
 
     uint32_t valuelist[] = { eventmask };
 
     uint32_t valuemask = XCB_CW_EVENT_MASK;
 
-    xcb_screen_t screen = screens[screenNum];
+    // create new window on given screen, map & flush
 
     xcb_window_t window = xcb_generate_id(connection);
 
